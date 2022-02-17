d4bb19946e795cff3b3dc20986a1364f31d1bebe
BrianNBN <brinbn@outlook.com>
2022-02-17 13:58:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import { lazy, Suspense } from 'react';
+import Role from './components/role';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router'
+
+// styles
+
+function App() {
+  return (
+    // <>role</>
+    <Role />
+  );
+}
+
+export default App;
